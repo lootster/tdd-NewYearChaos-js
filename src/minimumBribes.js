d1848729cf30141b3eae8e83bf118e45b9ee680f
Sheldon Lim <sheldon.lim@online-pajak.com>
2020-01-03 17:51:27 +0800

    Write 6th test case

--- src/minimumBribes.js
+++ src/minimumBribes.js
@@ -1,5 +1,25 @@
+const NO_BRIBE = 0;
+
 function minimumBribes(queue) {
-    return 0;
+	if (isEmptyOrOnlyOnePerson(queue)) {
+		return NO_BRIBE;
+	}
+	let counter = 0;
+	let max = queue[0];
+
+	for (let i = 1; i < queue.length; i++) {
+		if (max > queue[i]) {
+			counter += 1;
+		} else {
+			max = queue[i];
+		}
+    }
+    
+	if (counter <= 3 ) return counter;
 }
 
 module.exports = minimumBribes;
+
+function isEmptyOrOnlyOnePerson(queue) {
+	return !queue || queue.length < 2;
+}
--- test/minimumBribes.test.js
+++ test/minimumBribes.test.js
@@ -1,7 +1,41 @@
 const minimumBribes = require("../src/minimumBribes");
 
 describe("minimumBribes", () => {
+	it("should return 0 if there is no input", () => {
+		expect(minimumBribes()).toEqual(0);
+	});
 	it("should return 0 if no queue", () => {
-        expect(minimumBribes()).toEqual(0);
-    });
+		let queue = [];
+		expect(minimumBribes(queue)).toEqual(0);
+	});
+	it("should return 0 if there is one person in queue", () => {
+		let queue = [1];
+		expect(minimumBribes(queue)).toEqual(0);
+	});
+	it("should return 0 if there is 2 person in order", () => {
+		let queue = [1, 2];
+		expect(minimumBribes(queue)).toEqual(0);
+	});
+	it("should return 1 if there is 2 person in queue", () => {
+		let queue = [2, 1];
+		expect(minimumBribes(queue)).toEqual(1);
+	});
+	it("should return 0 if there is 3 person in order", () => {
+		let queue = [1, 2, 3];
+		expect(minimumBribes(queue)).toEqual(0);
+	});
+	it("should return 1 if there is 1 bribe", () => {
+        let queue = [1, 3, 2];
+		expect(minimumBribes(queue)).toEqual(1);
+		queue = [2, 1, 3];
+		expect(minimumBribes(queue)).toEqual(1);
+	});
+	it("should return 2 minimum bribe", () => {
+        let queue = [3, 1, 2];
+		expect(minimumBribes(queue)).toEqual(2);
+	});
+	it("should return 3 minimum bribe", () => {
+        let queue = [3, 2, 1];
+		expect(minimumBribes(queue)).toEqual(3);
+	});
 });
